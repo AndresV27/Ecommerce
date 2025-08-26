5cdb671f45bd95c3b4f6ea29eba568be3a22aa92
Andres Vasquez <andres.dominios27@gmail.com>
2025-08-25 20:02:33 -0600

    Subi el proyecto

---
+++ app/Models/Feature.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feature extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'value',
+        'option_id',
+        'description',
+
+    ];
+    //Relacion uno a muchos inversa
+    public function options(){
+        return $this->belongsTo(Option::class);
+    }
+    //Relacion muchos a muchos
+    public function variants(){
+        return $this->belongsToMany(Variant::class)
+        ->withTimestamps();
+    }
+}
