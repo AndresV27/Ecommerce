2b2ac3c77127cf0e312385c4a0634417bd347c80
Andres Vasquez <andres.dominios27@gmail.com>
2025-08-26 21:39:48 -0600

    Termine la plantilla admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,6 +12,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/94693ad428.js" crossorigin="anonymous"></script>
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -19,10 +22,20 @@
     @livewireStyles
 </head>
 
-<body class="font-sans antialiased" x-data="{
-    sidebarOpen: false
-}">
+    <body class="font-sans antialiased"
+        x-data="{
+        sidebarOpen: false
+    }"
+    :class="{
+        'overflow-y-hidden': sidebarOpen
+    }">
 
+    <div class="fixed inset-0 bg-gray-900 bg-opacity-50 z-20 sm:hidden" 
+    style="display: none;" 
+    x-show="sidebarOpen"
+    x-on:click="sidebarOpen= false">
+
+    </div>
 
 
     @include('layouts.partials.admin.navegation')
@@ -30,6 +43,8 @@
 
     <div class="p-4 sm:ml-64">
         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+                @include('layouts.partials.admin.breadcrumb')
+
             {{ $slot }}
         </div>
     </div>
